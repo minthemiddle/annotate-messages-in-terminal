e5e95948a687b1dfd5e075048dc4144df7532879
Martin Betz <martinmichaelbetz@gmail.com>
2024-02-08 20:06:54 +0100

    Add: comment option

--- annotate.py
+++ annotate.py
@@ -14,7 +14,8 @@ def count_unannotated_rows(df, language=None):
 @click.command()
 @click.argument('file', type=click.Path(exists=True))
 @click.option('--config', 'config_file', default='config.ini', show_default=True, help='Path to the configuration file.')
-def main(file, config_file):
+@click.option('--comment', is_flag=True, default=False, help='Prompt for a comment after each annotation.')
+def main(file, config_file, comment):
     """Script to annotate data."""
 
     config = configparser.ConfigParser()
@@ -27,6 +28,9 @@ def main(file, config_file):
 
     if 'label_class_human' not in df.columns:
         df['label_class_human'] = pd.NA
+
+    if 'comment' not in df.columns and comment:
+            df['comment'] = pd.NA
 
     language = click.prompt("Pick your language", type=click.Choice(language_choices), default="")
 
@@ -59,6 +63,9 @@ def main(file, config_file):
         elif len(choice) == 1 and choice.isdigit() and 0 < int(choice) <= len(categories_values_dict):
             choice_index = int(choice) - 1
             df.loc[idx, 'label_class_human'] = list(categories_values_dict.values())[choice_index]
+            if comment:
+                user_comment = click.prompt("Enter your comment", default="")
+                df.loc[idx, 'comment'] = user_comment
         else:
             console.print("Invalid choice.")
 
